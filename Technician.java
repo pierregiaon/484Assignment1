af823de97da752b88cdd43e8dbcada141cee6bc3
pierregiaon <44989143+pierregiaon@users.noreply.github.com>
2019-05-16 16:11:10 -0400

    Update Technician.java

--- Technician.java
+++ Technician.java
@@ -1,17 +1,25 @@
-package hw1;
+package HW1;
 
+import javafx.collections.FXCollections;
+
+import java.text.DecimalFormat;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.util.*;
 
 public class Technician {
-   
     public int techNum; 
-    public static int techIDCount = 0; 
-    public String technicianName; 
+    public static int nextTechID = 0; 
+    public String techName; 
+    public static ObservableList obsTech = FXCollections.observableArrayList();
+    
   
-    public Technician(String technicianName) {
-        this.technicianName = technicianName; 
-        this.techNum = techIDCount++; 
+    public Technician(String techName) {
+        this.techName = techName; 
+        this.techNum = nextTechID++; 
+        obsTech.add(this.techNum + ": " + this.techName);
     }
     public static int getTechIDCount() {
-        return techIDCount++; 
+        return nextTechID++; 
     }
 }
