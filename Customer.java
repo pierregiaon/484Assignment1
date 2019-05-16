48fce4bef89d7f2d54647256450d37762a72cf8f
pierregiaon <44989143+pierregiaon@users.noreply.github.com>
2019-05-16 16:10:40 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,11 +1,17 @@
 package HW1;
 
+import java.text.DecimalFormat;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.util.*;
+
 public class Customer {
     private int customerID;
     public String name;
     public String phone;
     public String address;
     public static int nextCustomerID = 0;
+    public static ObservableList obsCust = FXCollections.observableArrayList();
     
     public Customer(String name, String phone, String address)
     {
@@ -13,6 +19,7 @@ public class Customer {
         this.phone = phone;
         this.address = address;
         this.customerID = nextCustomerID++;
+        obsCust.add(this.customerID + ": " + this.name);
     }
     
     public String toString()
