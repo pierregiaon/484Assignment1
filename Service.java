6f74967b3ddcc7f3e06bc92db79933d726802114
meghanakrishna <krishnmx@dukes.jmu.edu>
2019-05-16 15:16:37 -0400

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package HW1;
+package hw1;
 
-/**
- *
- * @author pierregiaon
- */
+
 public class Service {
-    
+  public int serviceID; 
+  public static int serviceIDCount = 0; 
+  public String serviceName; 
+  public String serviceLevel; 
+  public String town; 
+  public Technician techNum; 
+  //public static Technician techNumCount = 0; 
+  
+  public Service(String serviceName, String serviceLevel, String town, Technician techNum) {
+      this.serviceName = serviceName; 
+      this.town = town; 
+      this.serviceID = serviceIDCount ++; 
+      this.serviceLevel = serviceLevel; 
+      //this.techNum = techNum.getTechIDCount();
+      //need to get techID to correctly incremate with tech count from the tech class 
+      //so it can work as like an employee id for technicians 
+  }
 }
