a9243460edb134f7af0ba6187b77a3e8497c53b8
meghanakrishna <krishnmx@dukes.jmu.edu>
2019-05-16 15:49:19 -0400

    Create store.java

---
+++ store.java
@@ -0,0 +1,12 @@
+package hw1;
+
+
+public class Store {
+    public int storeID; 
+    public static int storeIDCount = 0; 
+    public String storeName; 
+    
+    public Store(String storeName) {
+        this.storeName = storeName; 
+    }
+}
