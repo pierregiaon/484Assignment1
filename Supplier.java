0abf49de005d587edb3fb6198ede33471b859ff6
pierregiaon <44989143+pierregiaon@users.noreply.github.com>
2019-05-16 15:32:44 -0400

    Update Supplier.java

--- Supplier.java
+++ Supplier.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package HW1;
 
-/**
- *
- * @author pierregiaon
- */
 public class Supplier {
+    private int supplierID;
+    public String supplierName;
+    public String supplierAddress;
+    public String contactName;
+    public String contactPhone;
+    public String contactEmail;
+    public static int nextSupplierID = 0;
     
+    public Supplier(String supplierName, String supplierAddress, String contactName,
+            String contactPhone, String contactEmail)
+    {
+        this.supplierName = supplierName;
+        this.supplierAddress = supplierAddress;
+        this.contactName = contactName;
+        this.contactPhone = contactPhone;
+        this.contactEmail = contactEmail;
+        this.supplierID = nextSupplierID++;
+    }
 }
