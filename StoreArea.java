343d04ca0890fd3fe1fedbb6b2cc72d16404003a
pierregiaon <44989143+pierregiaon@users.noreply.github.com>
2019-05-16 16:31:35 -0400

    Update StoreArea.java

--- StoreArea.java
+++ StoreArea.java
@@ -1,18 +1,26 @@
-package hw1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HW1;
 
-
+/**
+ *
+ * @author pierregiaon
+ */
 public class StoreArea {
     public int storeAreaID; 
-    public static int storeAreaCount = 0; 
+    public static int nextStoreArea = 0; 
     public String serviceCenterName; 
-    public String serviceDep; 
-    public String depDesc; 
+    public String serviceDept; 
+    public String deptDesc; 
     
-    public StoreArea(String serviceCenterName, String serviceDep, String depDesc) {
-        this.depDesc = depDesc; 
+    public StoreArea(String serviceCenterName, String serviceDept, String deptDesc) {
+        this.deptDesc = deptDesc; 
         this.serviceCenterName = serviceCenterName; 
-        this.serviceDep = serviceDep; 
-        this.storeAreaID = 0 + storeAreaCount; 
+        this.serviceDept = serviceDept; 
+        this.storeAreaID = nextStoreArea++; 
     }
     
 }
