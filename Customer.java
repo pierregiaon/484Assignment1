774a0ab2dc434c909ae2b43a1a55b2e69678b8b7
meghanakrishna <krishnmx@dukes.jmu.edu>
2019-05-17 10:24:24 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -5,6 +5,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import java.util.*;
 
+
+
 public class Customer {
     private int customerID;
     public String name;
@@ -12,6 +14,10 @@ public class Customer {
     public String address;
     public static int nextCustomerID = 0;
     public static ObservableList obsCust = FXCollections.observableArrayList();
+    public ArrayList<Product> addProdOrder = new ArrayList<>(); 
+    public ArrayList<Service> addServiceOrder = new ArrayList<>(); 
+    public ArrayList<Store> addStoreOrdered = new ArrayList<>();
+    
     
     public Customer(String name, String phone, String address)
     {
@@ -21,6 +27,15 @@ public class Customer {
         this.customerID = nextCustomerID++;
         obsCust.add(this.customerID + ": " + this.name);
     }
+    public void addProdOrder(Product newProduct) {
+        addProdOrder.add(newProduct); 
+    }
+    public void addServiceOrder(Service newService) {
+        addServiceOrder.add(newService); 
+    }
+    public void addStoreOrdered(Store newStore) {
+        addStoreOrdered.add(newStore); 
+    }
     
     public String toString()
     {
@@ -29,5 +44,8 @@ public class Customer {
         
         return result;
     }
+   
     
+}
+
 }
