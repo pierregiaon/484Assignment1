307184cd7ae60ca0f72bb06ffb08ad3a7b12d798
meghanakrishna <krishnmx@dukes.jmu.edu>
2019-05-17 10:26:06 -0400

    Update store.java

--- store.java
+++ store.java
@@ -4,17 +4,26 @@ import java.text.DecimalFormat;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import java.util.*;
-
 public class Store {
+   
+
+
     public int storeID; 
     public static int nextStoreID = 0; 
     public String storeName; 
     public static ObservableList obsStore = FXCollections.observableArrayList();
+    public ArrayList<Technician> addStoreTech = new ArrayList<>(); 
     
+    public void addStoreTech(Technician newTechnician) {
+        addStoreTech.add(newTechnician); 
+    }
     
     public Store(String storeName) {
         this.storeName = storeName;
         this.storeID = nextStoreID++;
         obsStore.add(this.storeName);
     }
+    public String toString() {
+        return this.storeName; 
+}
 }
