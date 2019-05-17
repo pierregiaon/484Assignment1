b370c964ade7c23efb2fe4cbe107f51b6934eca6
pierregiaon <44989143+pierregiaon@users.noreply.github.com>
2019-05-16 20:47:14 -0400

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,22 +1,31 @@
-package hw1;
+package HW1;
 
+import java.text.DecimalFormat;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.util.*;
 
 public class Service {
-  public int serviceID; 
-  public static int serviceIDCount = 0; 
-  public String serviceName; 
-  public String serviceLevel; 
-  public String town; 
-  public Technician techNum; 
-  //public static Technician techNumCount = 0; 
-  
-  public Service(String serviceName, String serviceLevel, String town, Technician techNum) {
+    public int serviceID; 
+    public static int serviceIDCount = 0; 
+    public String serviceName; 
+    public String serviceLevel; 
+    public String town; 
+    public static ObservableList obsServ = FXCollections.observableArrayList();
+    
+  public Service(String serviceName, String serviceLevel, String town) {
       this.serviceName = serviceName; 
       this.town = town; 
       this.serviceID = serviceIDCount ++; 
       this.serviceLevel = serviceLevel; 
-      //this.techNum = techNum.getTechIDCount();
-      //need to get techID to correctly incremate with tech count from the tech class 
-      //so it can work as like an employee id for technicians 
+      obsServ.add(this.serviceID + ": " + this.serviceName);
+      
+  } 
+  
+  public String toString()
+  {
+    String result = "";
+    result += String.format("%3d\t\t\t\t%s\t\t\t%s\t\t\t%s", this.serviceID, this.serviceName, this.serviceLevel, this.town);
+    return result;
   }
 }
